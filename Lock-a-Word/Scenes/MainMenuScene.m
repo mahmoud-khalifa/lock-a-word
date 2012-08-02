9047970cf8542adf5a4cd35a09b6503724a4e314
Mohamed saleh <eng.mohamedszaied@gmail.com>
2012-08-02 17:51:18 +0200

    Modified the intro menu

--- Lock-a-Word/Scenes/MainMenuScene.m
+++ Lock-a-Word/Scenes/MainMenuScene.m
@@ -16,6 +16,21 @@
 // Nedded to instert the TapforTap Ads
 #import "TapForTap.h"
 
+
+@interface MainMenuScene()
+{
+    CCMenuItemImage *menuItemAnimate1; 
+    CCMenuItemImage *menuItemAnimate2;
+    CCMenuItemImage *menuItemAnimate3;
+    CGSize size;
+}
+
+-(void) animateMenuItems;
+-(void) hideMenuItems;
+-(void) showMainMenu;
+
+@end
+
 #pragma mark - MainMenuScene
 
 // MainMenuScene implementation
@@ -45,7 +60,7 @@
 	if( (self=[super init]) ) {
 		
         // Get the screen size
-        CGSize size =[[CCDirector sharedDirector] winSize];
+        size =[[CCDirector sharedDirector] winSize];
         
         // Creating an entry background image
         [CCTexture2D setDefaultAlphaPixelFormat:kCCTexture2DPixelFormat_RGB565];
@@ -53,35 +68,39 @@
         entrybackgroundImage.position =ccp(size.width/2, size.height/2);
         [self addChild:entrybackgroundImage];
         [CCTexture2D setDefaultAlphaPixelFormat:kCCTexture2DPixelFormat_RGBA4444];
+        [self animateMenuItems];
+        [self performSelector:@selector(hideMenuItems) withObject:nil afterDelay:2.0f];
+        [self performSelector:@selector(showMainMenu) withObject:nil afterDelay:2.0f];
+        
+         
+        
+        
+       
+        
+        
+        
+        
         
         // Creating Menu Items
         // Normal images and selected are the same 
         
-        CCMenuItemImage *menuItem1 =[CCMenuItemImage itemWithNormalImage:@"main_btn_play.png" selectedImage:@"main_btn_play.png" target:self selector:@selector(goToGameModeScene:)];
-          CCMenuItemImage *menuItem2 =[CCMenuItemImage itemWithNormalImage:@"main_btn_instr.png" selectedImage:@"main_btn_instr.png" target:self selector:@selector(goToInstructions)];
-          CCMenuItemImage *menuItem3=[CCMenuItemImage itemWithNormalImage:@"main_btn_full.png" selectedImage:@"main_btn_full.png" target:self selector:@selector(goTofull)];
+//        CCMenuItemImage *menuItem1 =[CCMenuItemImage itemWithNormalImage:@"main_btn_play.png" selectedImage:@"main_btn_play.png" target:self selector:@selector(goToGameModeScene:)];
+//          CCMenuItemImage *menuItem2 =[CCMenuItemImage itemWithNormalImage:@"main_btn_instr.png" selectedImage:@"main_btn_instr.png" target:self selector:@selector(goToInstructions)];
+//          CCMenuItemImage *menuItem3=[CCMenuItemImage itemWithNormalImage:@"main_btn_full.png" selectedImage:@"main_btn_full.png" target:self selector:@selector(goTofull)];
+//
+////        menuItem1.normalImage.position = ccp(size.width/2, size.height/2 - (.16 *size.height/2));
+////        menuItem2.position=ccp(size.width/2, size.height/2 - (.368 *size.height/2));
+////        menuItem3.position=ccp(size.width/2, size.height/2 - (.577 *size.height/2));
+//        
+//       
+//        // Adding items to the menu
+//        CCMenu *gameMenu = [CCMenu menuWithItems:menuItem1,menuItem2,menuItem3, nil];
+////        gameMenu.anchorPoint=CGPointZero;
+//        [gameMenu alignItemsVertically];
+//        gameMenu.position=ccp(size.width/2,size.height/2 - (.380 *size.height/2));      
+//       
+//        [self addChild:gameMenu];
 
-//        menuItem1.normalImage.position = ccp(size.width/2, size.height/2 - (.16 *size.height/2));
-//        menuItem2.position=ccp(size.width/2, size.height/2 - (.368 *size.height/2));
-//        menuItem3.position=ccp(size.width/2, size.height/2 - (.577 *size.height/2));
-        
-       
-        // Adding items to the menu
-        CCMenu *gameMenu = [CCMenu menuWithItems:menuItem1,menuItem2,menuItem3, nil];
-//        gameMenu.anchorPoint=CGPointZero;
-        [gameMenu alignItemsVertically];
-        gameMenu.position=ccp(size.width/2,size.height/2 - (.380 *size.height/2));      
-       
-        [self addChild:gameMenu];
-        
-//        // This is for TapforTap
-//        TapForTapAdView *adView = [[TapForTapAdView alloc] initWithFrame: CGRectMake(0, 0, 320, 50)];
-//        [[[CCDirector sharedDirector] view] addSubview: adView];
-//        
-//        // You don't have to do this if you set the default app ID in your app delegate
-//        adView.appId = @"<YOUR APP ID>";
-//        
-//        [adView loadAds];
 	}
 	return self;
 }
@@ -93,6 +112,67 @@
     [[CCDirector sharedDirector] pushScene:[GameModesScene scene]];
 }
 
+
+
+-(void)hideMenuItems {
+    menuItemAnimate1.visible=NO;
+    menuItemAnimate2.visible=NO;
+    menuItemAnimate3.visible=NO;
+}
+
+// This method will show our main menu
+-(void)animateMenuItems
+{
+    // Here I will add my begining of the intro animation "LOCK - A - WORD"
+    menuItemAnimate1 =[CCMenuItemImage itemWithNormalImage:@"btn_lock.png" selectedImage:@"btn_lock.png"];
+    menuItemAnimate2 =[CCMenuItemImage itemWithNormalImage:@"btn_a.png" selectedImage:@"btn_a.png"];
+    menuItemAnimate3 =[CCMenuItemImage itemWithNormalImage:@"btn_word.png" selectedImage:@"btn_word.png"];
+    
+    
+    // Adding "LOCK - A - WORD" items to the menu
+    CCMenu *introMenu = [CCMenu menuWithItems:menuItemAnimate1,menuItemAnimate2,menuItemAnimate3, nil];
+    // gameMenu.anchorPoint=CGPointZero;
+    [introMenu alignItemsVertically];
+    introMenu.position=ccp(size.width/2,size.height/2 - (.380 *size.height/2));      
+    
+    [self addChild:introMenu];
+    
+    // Here is the animation part of the LOCK-A-WORD Menu
+    menuItemAnimate1.scale=.75;        
+    [menuItemAnimate1 runAction:[CCScaleTo actionWithDuration:1.0 scale:1.0]];
+    menuItemAnimate2.scale=.75;
+    [menuItemAnimate2 runAction:[CCScaleTo actionWithDuration:1.5 scale:1.0]];
+    menuItemAnimate3.scale=.75;
+    [menuItemAnimate3 runAction:[CCScaleTo actionWithDuration:2.0 scale:1.0]];  
+    
+}
+
+// This method will be used to show "PLAY-INSTRUCTION-FULL" menu items
+-(void)showMainMenu 
+{
+    // Here I will add my begining of the intro animation "PLAY - INSTRUCTION - FULL"
+    CCMenuItemImage* mainMenuItem1 =[CCMenuItemImage itemWithNormalImage:@"main_btn_play.png" selectedImage:@"main_btn_play.png" target:self selector:@selector(goToGameModeScene:)];
+    CCMenuItemImage* mainMenuItem2 =[CCMenuItemImage itemWithNormalImage:@"main_btn_instr.png" selectedImage:@"main_btn_instr.png" target:self selector:@selector(goToInstructions)];
+    CCMenuItemImage* mainMenuItem3 =[CCMenuItemImage itemWithNormalImage:@"main_btn_full.png" selectedImage:@"main_btn_full.png" target:self selector:@selector(goTofull)];
+    
+    
+   
+    // Adding "PLAY - INSTRUCTION - FULL" items to the menu
+    CCMenu *mainMenu = [CCMenu menuWithItems:mainMenuItem1,mainMenuItem2,mainMenuItem3, nil];
+    // gameMenu.anchorPoint=CGPointZero;
+    [mainMenu alignItemsVertically];
+    mainMenu.position=ccp(size.width/2,size.height/2 - (.380 *size.height/2));      
+    
+    [self addChild:mainMenu];
+    
+    // Here is the animation part of the LOCK-A-WORD Menu
+    mainMenuItem1.scale=.75;        
+    [mainMenuItem1 runAction:[CCScaleTo actionWithDuration:1.0 scale:1.0]];
+    mainMenuItem2.scale=.75;
+    [mainMenuItem2 runAction:[CCScaleTo actionWithDuration:1.5 scale:1.0]];
+    mainMenuItem3.scale=.75;
+    [mainMenuItem3 runAction:[CCScaleTo actionWithDuration:2.0 scale:1.0]];    
+}
 
 // on "dealloc" you need to release all your retained objects
 - (void) dealloc
--- Lock-a-Word/main.m
+++ Lock-a-Word/main.m
@@ -1,4 +1,4 @@
-//
+
 //  main.m
 //  Lock-A-Word
 //
